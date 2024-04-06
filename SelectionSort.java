aed805bee367d9ef12aa98c0f6904d03a6df1a0b
koo <rnwnsgur28@gmail.com>
2024-04-06 22:05:32 +0900

    first

---
+++ SelectionSort.java
@@ -0,0 +1,20 @@
+public class SelectionSort extends AbstractSort {
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        for(int i = 0; i < N - 1; i++) {
+            int min = i;
+            for(int j = i + 1; j < N; j++) {
+                if(less(a[j], a[min]))
+                    min = j;
+            }
+            exch(a, i, min);
+        }
+        assert isSorted(a);
+    }
+
+    public static void main(String[] args) {
+        Integer[] a = {5, 7, 8, 2, 3};
+        SelectionSort.sort(a);
+        SelectionSort.show(a);
+    }
+} 
---
+++ algo-ch1-ex/CountingSort.java
@@ -0,0 +1,19 @@
+public class CountingSort {
+    public static int[] sort(int[] A, int K) {
+        int i, N = A.length;
+        int[] C = new int[K], B = new int[N];
+
+        for (i = 0; i < N; i++) C[A[i]]++;
+        for (i = 1; i < K; i++) C[i] += C[i - 1];
+        for (i = N - 1; i >= 0; i--) B[--C[A[i]]] = A[i];
+        return B;
+    }
+
+    public static void main(String[] args) {
+        int[] A = {10, 4, 5, 8, 1, 3, 6}, B;
+        B = CountingSort.sort(A, 11);
+
+        for(int i = 0; i < B.length; i++)
+            System.out.print(B[i] + " ");
+    }
+}
